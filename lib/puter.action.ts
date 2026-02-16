45a21413eb9cb24be33fe9e6b25b56539aa9ae1c
Zeineb Haraketi <zeinebharaketi298@gmail.com>
2026-02-16 19:52:42 +0000

    Implementing the drag and drop upload

--- lib/puter.action.ts
+++ lib/puter.action.ts
@@ -1,13 +1,27 @@
 import puter from "@heyputer/puter.js";
+import { PUTER_WORKER_URL } from "./constants";
 
 export const signIn = async () => await puter.auth.signIn();
 
 export const signOut = () => puter.auth.signOut();
 
 export const getCurrentUser = async () => {
-    try {
-        return await puter.auth.getUser();
-    } catch {
-        return null;
-    }
-}
+  try {
+    return await puter.auth.getUser();
+  } catch {
+    return null;
+  }
+};
+
+export const createProject = async ({
+  item,
+  visibility = "private",
+}: CreateProjectParams): Promise<DesignItem | null | undefined> => {
+  if (!PUTER_WORKER_URL) {
+    console.warn("Missing VITE_PUTER_WORKER_URL; skip history fetch;");
+    return null;
+  }
+
+      const projectId = item.id;
+
+};
