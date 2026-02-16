f9759555c778dfef2489874f95a9f5b568e3fb57
Zeineb Haraketi <zeinebharaketi298@gmail.com>
2026-02-16 19:22:52 +0000

    Implementing the homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,12 +11,94 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const timeStamp = Date.now();
+
   return (
     <div className="home">
       <Navbar />
-      <h1 className="text-3xl text-indigo-700 font-extrabold">
-      Welcome to the Home Page
-    </h1>
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing VizioBuild 2.0</p>
+        </div>
+
+        <h1>Visualize tomorrow's spaces today with VizioBuild.</h1>
+
+        <p className="subtitle">
+          VizioBuild is an A next-gen architectural workspace where AI
+          accelerates creativity from sketch to final render.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Where your projects and community creations come together.</p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFuZHNjYXBlJTIwZGVzaWdufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+                  alt="Project"
+                />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+
+              <div className="card-body">
+                <h3>Modern Villa</h3>
+
+                <div className="meta">
+                  <Clock size={12} />
+                  <span>{new Date(timeStamp).toLocaleDateString()}</span>
+                  <span>By ZeinebHa</span>
+                </div>
+
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
